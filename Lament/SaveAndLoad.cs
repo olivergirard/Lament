91bd6680e8ddc75f891ba2a5f878032b620b9866
olivergirard <girard1@purdue.edu>
2022-08-12 14:38:53 -0400

    Add project files.

---
+++ Lament/SaveAndLoad.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Lament
+{
+    public class SaveAndLoad
+    {
+
+        public struct SaveData
+        {
+            public int num { get; set; }
+        }
+
+        public static SaveData LoadGame()
+        {
+            string gamePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Lament");
+
+            if ((File.Exists(gamePath)) == false)
+            {
+                Directory.CreateDirectory(gamePath);
+            }
+
+
+            SaveData saveGameData = new SaveData();
+            string savePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Lament", "save.json");
+
+            if (File.Exists(savePath))
+            {
+                saveGameData = JsonSerializer.Deserialize<SaveData>(File.ReadAllText(savePath), options);
+            }
+            else
+            {
+                saveGameData.num = 0;
+            }
+
+            return saveGameData;
+        }
+
+        public static void SaveGame(SaveData saveFile)
+        {
+            string saveData = JsonSerializer.Serialize(saveFile, options);
+            string savePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Lament", "save.json");
+            File.WriteAllText(savePath, saveData);
+        }
+
+        /* Makes the save data .json file human-readable. */
+
+        private static JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true,
+        };
+    }
+}
---
+++ Lament/StartGame.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Drawing;
+using System.Text.Json;
+
+namespace Lament
+{
+    public class StartGame : Game
+    {
+
+        private GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont font;
+        SaveAndLoad.SaveData save;
+
+        public StartGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            save = SaveAndLoad.LoadGame();
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            font = Content.Load<SpriteFont>(@"C:\Users\azure\source\repos\Lament\Content\bin\DesktopGL\Content\font");
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Microsoft.Xna.Framework.Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            string output = "" + save.num;
+            Vector2 fontorigin = font.MeasureString(output) / 2;
+            spriteBatch.DrawString(font, output, fontorigin, Microsoft.Xna.Framework.Color.Blue);
+            spriteBatch.End();
+        }
+        protected override void OnExiting(object sender, EventArgs args)
+        {
+            (save.num)++;
+            SaveAndLoad.SaveGame(save);
+            base.OnExiting(sender, args);
+        }
+    }
+}
